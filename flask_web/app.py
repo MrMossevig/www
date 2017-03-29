f0fd0a3c30fd336e6fbcdca9b3d752bd33fa1f09
Alf Petter Mossevig <taperkatt@gmail.com>
2017-03-29 16:14:38 +0200

    First functional framework

--- flask_web/app.py
+++ flask_web/app.py
@@ -1,11 +1,19 @@
 # flask_web/app.py
 
-from flask import Flask
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+def index():
+    return render_template('index.html', url=url_for('index_theWebAge'))
+
+@app.route('/projects/')
+def index_projects():
+    return 'Proects Page'
+
+@app.route('/projects/TheMathAge')
+def index_theWebAge():
+    return 'TheWebAge'
 
 
 if __name__ == '__main__':
